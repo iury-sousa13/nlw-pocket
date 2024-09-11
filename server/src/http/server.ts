59e90b747f0476194f34be0d8102ab250f2c04e7
Iury Sousa <iurysousa13@outlook.com>
2024-09-10 18:41:00 -0600

    configure biome code formatter

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,9 +1,9 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify({ logger: true });
+const app = fastify({ logger: true })
 
 app
   .listen({
     port: 3001,
   })
-  .then(() => console.log("HTTP server in running!"));
+  .then(() => console.log('HTTP server in running!'))
