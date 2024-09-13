198728a7fbd1da3dd31b7d0c03db9254ddfe8044
Iury Sousa <iurysousa13@outlook.com>
2024-09-12 20:39:56 -0600

    create home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,19 @@
+import { Plus } from 'lucide-react'
+import logo from './assets/in-orbit-logo.svg'
+import letsStart from './assets/rocket-launch-illustration.svg'
+import { Button } from './components/ui/button'
+
 export function App() {
   return (
-    <>
-      <h1 className="text-xl">Ola</h1>
-    </>
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <Button>
+        <Plus className="size-4" /> Cadastrar meta
+      </Button>
+    </div>
   )
 }
